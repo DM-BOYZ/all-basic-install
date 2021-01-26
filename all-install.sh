e1441274ed7b066cb5d06e88e578f28ef2a92184
DM-BOYZ <71525581+DM-BOYZ@users.noreply.github.com>
2021-01-26 12:47:41 +0600

    Add files via upload

---
+++ all-install.sh
@@ -0,0 +1,42 @@
+#all in one basic tool installer..
+
+
+echo ""
+echo "-----------------------"
+echo "|      all tool        |"
+echo "-----------------------"
+echo "======================="
+echo "|    coded by DM       |"
+echo "======================="
+
+
+echo "installing now...."
+
+apt update
+apt upgrade -y
+apt install python
+apt install python2
+apt install git
+apt install wget php curl
+apt install openssh
+apt install ruby
+apt install figlet
+gem install lolcat
+apt install zziplib
+apt install zsync
+apt install zstd-static
+apt install w3m
+apt install w3m-img
+apt install perl
+apt install apache2
+apt install clang
+apt install php
+pip2 install mechanize
+pip2 install requests
+pip2 install bs4
+
+
+clear
+
+
+echo "now you are ready to use.."
